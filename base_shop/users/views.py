3094cdbc1efd698e5a20b01a4652f3a1c27a1be3
Tsurumi Tatsuya <93573854+tatsuya-tsurumi@users.noreply.github.com>
2025-05-22 09:31:00 +0900

    ログイン機能実装

--- base_shop/users/views.py
+++ base_shop/users/views.py
@@ -5,6 +5,7 @@ TemplateView, CreateView, FormView, View
 from django.contrib.auth import authenticate, login, logout
 from .forms import RegistForm, UserLoginForm
 from django.urls import reverse_lazy
+from django.contrib import messages
 
 class HomeView(TemplateView):
   template_name = 'home.html'
@@ -17,7 +18,23 @@ class RegistUserView(CreateView):
 class UserLoginView(FormView):
   template_name = 'user_login.html'
   form_class = UserLoginForm
-  success_url = reverse_lazy('accounts:home')
+  success_url = reverse_lazy('users:home')
+
+  def form_valid(self, form):
+    email = form.cleaned_data['email']
+    password = form.cleaned_data['password']
+    user = authenticate(email=email, password=password)
+    if user:
+      login(self.request, user)
+      return super().form_valid(form)
+    else:
+      messages.error(self.request, 'メールアドレスまたはパスワードが間違っています')
+      return self.form_invalid(form)
+    
 
 class UserLogoutView(View):
-  pass
+  def get(self, request, *args, **kwargs):
+    return render(request, 'user_logout.html')
+  def post(self, request, *args, **kwargs):
+    logout(request)
+    return redirect('users:home')
