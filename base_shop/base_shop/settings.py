11342cfd17f7066884292d294e17b8b657c004c2
Tsurumi Tatsuya <93573854+tatsuya-tsurumi@users.noreply.github.com>
2025-05-22 10:12:24 +0900

    商品管理機能作成開始

--- base_shop/base_shop/settings.py
+++ base_shop/base_shop/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'items',
 ]
 
 MIDDLEWARE = [
@@ -110,7 +111,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static_local" ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media_local"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- base_shop/base_shop/urls.py
+++ base_shop/base_shop/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from django.conf.urls.static import static # 画像用
+from django.conf import settings # 画像用
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # 画像用
