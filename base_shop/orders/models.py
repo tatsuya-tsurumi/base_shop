d369bd01d17b9303dd6b0e32f9decf1457884f6a
Tsurumi Tatsuya <93573854+tatsuya-tsurumi@users.noreply.github.com>
2025-05-23 11:20:28 +0900

    カートmodel・view作成完了

--- base_shop/orders/models.py
+++ base_shop/orders/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.conf import settings
+from products.models import Product
 
 # Create your models here.
+class Cart(models.Model):
+  user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.user.username}のカート"
+
+class CartItem(models.Model):
+  cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+  product = models.ForeignKey(Product, on_delete=models.CASCADE)
+  quantity = models.PositiveIntegerField(default=1)
+
+  def __str__(self):
+    return f"{self.product.name} x {self.quantity}"
+
+class Order(models.Model):
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return f"注文{self.id} ({self.user.username})"
+  
+class OrderItem(models.Model):
+  order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+  product = models.ForeignKey(Product, on_delete=models.CASCADE)
+  quantity = models.PositiveIntegerField()
+
+  def __str__(self):
+    return f"{self.product.name} x {self.quantity}"
--- base_shop/orders/views.py
+++ base_shop/orders/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Cart, CartItem, Order, OrderItem
+from products.models import Product
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# カート表示
+@login_required
+def view_cart(request):
+  cart, created = Cart.objects.get_or_create(user=request.user)
+  return render(request, 'order/cart.html', {'cart':cart})
+
+# カートに商品追加
+@login_required
+def add_to_cart(request, product_id):
+  product = get_object_or_404(Product, id=product_id)
+  cart, created = Cart.objects.get_or_create(user=request.user)
+  item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+  item.quantity += 1
+  item.save()
+  return redirect('orders:view_cart')
+
+# カートから商品削除
+@login_required
+def remove_from_cart(request, item_id):
+  item = get_object_or_404(CartItem, id=item_id)
+  item.delete()
+  return redirect('orders: view_cart')
+
+# 購入処理。購入後カート内商品は削除
+@login_required
+def checkout(request):
+  cart = Cart.objects.get(user=request.user)
+  if not cart.items.exists():
+    return redirect('orders: view_cart')
+  
+  order = Order.objects.create(user=request.user)
+
+  for item in cart.items.all():
+    OrderItem.objects.create(
+      order = order,
+      product = item.product,
+      quantity = item.quantity
+    )
+
+    cart.products.all().delete()
+
+    return render(request, 'orders/checkout_success.html', {'order':order})
