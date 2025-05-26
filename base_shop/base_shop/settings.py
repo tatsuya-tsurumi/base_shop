8c59e8c8c754fb26c786a5b3ff80d442a617a522
Tsurumi Tatsuya <93573854+tatsuya-tsurumi@users.noreply.github.com>
2025-05-26 10:47:08 +0900

    CSS調整開始、ヘッダー・フッター調整

--- base_shop/base_shop/settings.py
+++ base_shop/base_shop/settings.py
@@ -109,6 +109,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'base_shop/static'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
